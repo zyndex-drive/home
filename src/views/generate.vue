b004d0be2573798a6b3953788463ca63f360e7c1
Shan.tk <tksudharshan@gmail.com>
2020-11-13 09:04:46 +0530

    Improved UI in Generator Page.

--- src/views/generate.vue
+++ src/views/generate.vue
@@ -1,174 +1,267 @@
 <template>
-  <div class="content mx-2 my-2">
-    <h5 class="has-text-centered subtitle">Generate Your Template Now</h5>
+  <div class="content mx-2 px-3 my-2">
+    <div class="text-center my-3 text-h6 font-weight-bold">
+      Generate Your Ultimate Index Now 😜
+    </div>
     <div class="columns is-multiline is-centered">
       <div class="column is-half my-2">
-        <form autocomplete="off">
-          <div class="columns is-desktop is-multiline is-centered">
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-form-textbox"
-                dense
-                v-model="name"
-                :persistent-hint="true"
-                :success="validations.name(name)"
-                hint="Enter the Website's Name of Your Choice "
-                label="Enter Site Name"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-half">
-              <v-select
-                prepend-icon="mdi-water-plus-outline"
-                dense
-                v-model="selTheme"
-                :persistent-hint="true"
-                :success="validations.selTheme(selTheme)"
-                :items="themes"
-                item-text="value"
-                item-value="value"
-                label="Select Your Theme"
-                hint="Please See the Screenshots for Theme's Look."
-                outlined
-              ></v-select>
-            </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-web"
-                dense
-                v-model="fronturl"
-                :persistent-hint="true"
-                :success="validations.fronturl(fronturl)"
-                hint="Enter the Frontend Website URL to which it will be Deployed. Do Not Put / at the End."
-                label="Frontend URL"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-webhook"
-                dense
-                v-model="version"
-                :persistent-hint="true"
-                :success="validations.version()"
-                hint="Latest Version will be Served"
-                :disabled="true"
-                label="Version"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-web"
-                dense
-                v-model="backurl"
-                :persistent-hint="true"
-                :success="validations.backurl(backurl)"
-                hint="If You have not Deployed the Backend. Please see the Docs."
-                label="Backend URL"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-lock-question"
-                dense
-                v-model="hybridpass"
-                :persistent-hint="true"
-                :success="validations.hybridpass(hybridpass)"
-                hint="If You have Deployed the Backend and Generated Put Here or Leave it Blank"
-                label="Hybrid Password"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-full">
-              <div class="columns is-multiline is-centered is-vcentered">
-                <div
-                  :class="
-                    ismobile
-                      ? 'column is-one-third has-text-centered'
-                      : 'column is-one-third'
-                  "
-                >
-                  <v-btn small color="info" @click="openAuthWindow">
-                    Get Authorization Code
-                  </v-btn>
+        <form>
+          <v-card elevation="10" class="pa-4 my-2">
+            <div class="text-center text-h6 ma-3">Basic Details</div>
+            <div class="columns is-desktop is-multiline is-centered">
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-form-textbox"
+                  dense
+                  autocomplete="off"
+                  type="text"
+                  v-model="name"
+                  :persistent-hint="true"
+                  :success="validations.name(name)"
+                  hint="Enter the Website's Name of Your Choice "
+                  label="Enter Site Name"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-select
+                  prepend-icon="mdi-water-plus-outline"
+                  dense
+                  v-model="selTheme"
+                  autocomplete="off"
+                  :persistent-hint="true"
+                  :success="validations.selTheme(selTheme)"
+                  :items="themes"
+                  item-text="value"
+                  item-value="value"
+                  label="Select Your Theme"
+                  hint="Please See the Screenshots for Theme's Look."
+                  outlined
+                ></v-select>
+              </div>
+              <div class="column is-half">
+                <v-select
+                  prepend-icon="mdi-shape"
+                  dense
+                  v-model="variant"
+                  autocomplete="off"
+                  :persistent-hint="true"
+                  :success="validations.selTheme(variant)"
+                  :items="variants"
+                  item-text="value"
+                  item-value="value"
+                  label="Select Your Theme Variant"
+                  hint="Please See the Screenshots for Look."
+                  outlined
+                ></v-select>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-web"
+                  dense
+                  v-model="fronturl"
+                  autocomplete="off"
+                  type="url"
+                  :persistent-hint="true"
+                  :success="validations.fronturl(fronturl)"
+                  hint="Enter the Frontend Website URL to which it will be Deployed. Do Not Put / at the End."
+                  label="Frontend URL"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-webhook"
+                  dense
+                  v-model="version"
+                  autocomplete="off"
+                  :persistent-hint="true"
+                  :success="validations.version()"
+                  hint="Latest Version will be Served. Not Fillable"
+                  :disabled="true"
+                  label="Version"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-web"
+                  dense
+                  v-model="backurl"
+                  type="url"
+                  autocomplete="off"
+                  :persistent-hint="true"
+                  :success="validations.backurl(backurl)"
+                  hint="If You have not Deployed the Backend. Please see the Docs."
+                  label="Backend URL"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-lock-question"
+                  dense
+                  v-model="hybridpass"
+                  autocomplete="off"
+                  type="password"
+                  :persistent-hint="true"
+                  :success="validations.hybridpass(hybridpass)"
+                  hint="If You have Deployed the Backend and Generated Put Here or Leave it Blank"
+                  label="Hybrid Password"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half text-center">
+                <v-btn small color="primary" @click="openAuthWindow">
+                  Get Authorization Code
+                </v-btn>
+              </div>
+              <div class="column is-full">
+                <v-text-field
+                  prepend-icon="mdi-passport"
+                  dense
+                  v-model="authCode"
+                  type="password"
+                  :persistent-hint="true"
+                  autocomplete="off"
+                  :success="validations.authCode(authCode)"
+                  hint="Enter the Auth Code that You Generated with the Above Button"
+                  label="Authorization Code / Refresh Token"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-card-account-details-outline"
+                  dense
+                  v-model="clientId"
+                  type="password"
+                  :persistent-hint="true"
+                  autocomplete="off"
+                  :success="validations.name(clientId)"
+                  hint="Enter Your Client ID that was given in Google developer Console"
+                  label="Client ID"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-half">
+                <v-text-field
+                  prepend-icon="mdi-card-account-details"
+                  dense
+                  v-model="clientSec"
+                  type="password"
+                  :persistent-hint="true"
+                  autocomplete="off"
+                  :success="validations.name(clientSec)"
+                  hint="Enter Your Client Secret that was given in Google developer Console"
+                  label="Client Secret"
+                  hide-details="auto"
+                  outlined
+                ></v-text-field>
+              </div>
+              <div class="column is-full">
+                <div class="text-subtitle-1 font-weight-bold">
+                  <v-icon>mdi-square-root-box</v-icon> Roots
                 </div>
-                <div class="column is-two-thirds">
-                  <v-text-field
-                    prepend-icon="mdi-passport"
-                    dense
-                    v-model="authCode"
-                    type="password"
-                    :persistent-hint="true"
-                    :success="validations.authCode(authCode)"
-                    hint="Enter the Auth Code that You Generated with the Button"
-                    label="Authorization Code"
-                    :hide-details="true"
-                    outlined
-                  ></v-text-field>
-                </div>
+                <v-card shaped outlined class="py-2 px-2">
+                  <div
+                    v-if="roots.length < 1"
+                    class="text-center text-subtitle-2"
+                  >
+                    Enter Roots Information with the Below Button
+                  </div>
+                  <v-row v-for="(root, index) in roots" v-bind:key="index">
+                    <v-col
+                      v-if="roots.length > 0"
+                      cols="1"
+                      justify="center"
+                      class="text-subtitle-2"
+                    >
+                      {{ index + 1 }}
+                    </v-col>
+                    <v-col
+                      v-if="roots.length > 0"
+                      :cols="ismobile ? 8 : 4"
+                      justify="center"
+                      class="text-subtitle-2"
+                    >
+                      {{ root.name }}
+                    </v-col>
+                    <v-col
+                      v-if="roots.length > 0 && !ismobile"
+                      cols="4"
+                      justify="center"
+                      class="text-subtitle-2"
+                    >
+                      Protect File Link:
+                      {{ root.protect_file_link ? 'On' : 'Off' }}
+                    </v-col>
+                    <v-col
+                      v-if="roots.length > 0"
+                      cols="3"
+                      justify="center"
+                      class="text-center text-subtitle-2"
+                    >
+                      <v-btn small icon color="primary"
+                        ><v-icon>mdi-circle-edit-outline</v-icon></v-btn
+                      >
+                      <v-btn small icon color="primary"
+                        ><v-icon>mdi-delete</v-icon></v-btn
+                      >
+                    </v-col>
+                  </v-row>
+                  <div class="text-center text-subtitle-2">
+                    <v-btn color="primary"
+                      ><v-icon>mdi-plus</v-icon> Add Root</v-btn
+                    >
+                  </div>
+                </v-card>
               </div>
             </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-card-account-details-outline"
-                dense
-                v-model="clientId"
-                :persistent-hint="true"
-                :success="validations.name(clientId)"
-                hint="Enter Your Client ID that was given in Google developer Console"
-                label="Client ID"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-            <div class="column is-half">
-              <v-text-field
-                prepend-icon="mdi-card-account-details"
-                dense
-                v-model="clientSec"
-                :persistent-hint="true"
-                :success="validations.name(clientSec)"
-                hint="Enter Your Client Secret that was given in Google developer Console"
-                label="Client Secret"
-                hide-details="auto"
-                outlined
-              ></v-text-field>
-            </div>
-          </div>
+          </v-card>
+          <v-card elevation="10" class="pa-4 my-2">
+            <div class="text-center text-h6 ma-3">Other Options</div>
+          </v-card>
         </form>
       </div>
       <div class="column is-half">
         <v-container fluid>
-          <div class="has-text-right">
-            <v-btn text class="has-text-right" @click="summa"
-              >Copy <v-icon>mdi-content-copy</v-icon></v-btn
-            >
-          </div>
-          <v-textarea
-            class="is-maximum"
-            no-resize
-            height="100%"
-            :full-width="true"
-            :persistent-hint="true"
-            readonly
-            :disabled="true"
-            label="Your Template Goes Here"
-            :filled="true"
-            :outlined="true"
-          ></v-textarea>
+          <v-row>
+            <v-col cols="12" :class="ismobile ? 'text-center' : 'text-right'">
+              <v-btn color="primary" @click="summa"
+                >Copy <v-icon>mdi-content-copy</v-icon></v-btn
+              >
+            </v-col>
+            <v-col cols="12">
+              <v-textarea
+                class="is-maximum"
+                no-resize
+                height="100%"
+                :full-width="true"
+                :persistent-hint="true"
+                readonly
+                autocomplete="off"
+                :disabled="true"
+                label="Your Template Goes Here"
+                :filled="true"
+                :outlined="true"
+              ></v-textarea>
+            </v-col>
+          </v-row>
         </v-container>
       </div>
     </div>
   </div>
 </template>
 <script>
+import { mobileChecker } from '../plugins/helpers';
 export default {
   data: function () {
     return {
@@ -219,15 +312,41 @@ export default {
           value: 'witch-haze',
         },
       ],
+      variants: [
+        {
+          value: 'gradient',
+        },
+        {
+          value: 'black',
+        },
+      ],
       name: '',
       selTheme: '',
       fronturl: '',
       version: 'latest',
       backurl: '',
       hybridpass: '',
+      roots: [
+        {
+          folder_id: '12askjdbaskdbakdb',
+          name: 'Glory to Heaven',
+          protect_file_link: true,
+        },
+        {
+          folder_id: '12askjdbaskdbakdb',
+          name: 'Glory to Heaven',
+          protect_file_link: true,
+        },
+        {
+          folder_id: '12askjdbaskdbakdb',
+          name: 'Glory to Heaven',
+          protect_file_link: true,
+        },
+      ],
       authCode: '',
       clientId: '',
       clientSec: '',
+      variant: '',
       validations: {
         name: function (input) {
           if (input.length > 5) {
@@ -281,6 +400,7 @@ export default {
   },
   methods: {
     summa() {
+      console.log(this.roots.length);
       console.log('pressed');
     },
     openAuthWindow() {
@@ -292,12 +412,7 @@ export default {
   },
   computed: {
     ismobile() {
-      var width = window.innerWidth > 0 ? window.innerWidth : screen.width;
-      if (width > 966) {
-        return false;
-      } else {
-        return true;
-      }
+      return mobileChecker();
     },
   },
 };
